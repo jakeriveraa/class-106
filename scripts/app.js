e24e3902f65162f21b305cbe0dbd38661bad27e4
jakeriveraa <jake.riveraa@gmail.com>
2025-08-21 17:33:02 -0700

    assignment 2

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,29 @@
-function hello() {
-    console.log("hello there");
-    goodbye();
+let isVisible = true; // global avriable to track visibility
+
+function toggleVisibility() {
+    if (isVisible) {
+        $("#form").fadeOut(); // hide the form section
+        isVisible = false; // update the visibilty state
+    }else{
+        $("#form").fadeIn(); // show the form section
+        isVisible = true; // update the visibility state
+    }
 }
 
-function goodbye() {
-    console.log("bye!");
+function saveTask() {
+  console.log("hellow im the save task");
+  // get the value of the input
+
+  //create a new task object
+
+  // add the task to the list
 }
 
 function init() {
-    console.log("hello im the init");
-    hello();
+  console.log("hello im the init");
+  // hooks
+  $("#btnAdd").click(saveTask);
+  $("#btnDetails").click(toggleVisibility);
 }
 
 //New comment
